aaa84ce7ddcf650c8abc7f4164ff09ca71b5739d
feirou <1729013657@qq.com>
2020-07-10 22:19:57 +0800

    Site updated: 2020-07-10 22:19:55

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,12 +3,12 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "*/images/astronaut32.png");
+         $('[rel="icon"]').attr('href', "/images/astronaut32.png");
          document.title = '（づ￣3￣）づ╭❤～ 快回来!!!';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "*/images/astronaut32.png");
+         $('[rel="icon"]').attr('href', "/images/astronaut32.png");
          document.title = '─=≡Σ((( つ•̀ω•́)つ动感光波哔哔哔!!!' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
