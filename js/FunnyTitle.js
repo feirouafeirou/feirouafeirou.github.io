5ecfd20709c01bb2717486ad530fc40cfdd6e6d3
feirou <1729013657@qq.com>
2020-07-10 18:31:25 +0800

    Site updated: 2020-07-10 18:31:23

---
+++ js/FunnyTitle.js
@@ -0,0 +1,17 @@
+<!--浏览器搞笑标题-->
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "*/images/astronaut32.png");
+         document.title = '（づ￣3￣）づ╭❤～ 快回来!!!';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "*/images/astronaut32.png");
+         document.title = '─=≡Σ((( つ•̀ω•́)つ动感光波哔哔哔!!!' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
