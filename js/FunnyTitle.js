263a9644ce4f2f44a2afce45cdc0ab60589a021e
feirou <1729013657@qq.com>
2020-07-11 09:51:59 +0800

    Site updated: 2020-07-11 09:51:56

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/images/astronaut32.png");
-         document.title = '（づ￣3￣）づ╭❤～ 快回来!!!';
+         document.title = '_(:з」∠)_';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/images/astronaut32.png");
-         document.title = '─=≡Σ((( つ•̀ω•́)つ动感光波哔哔哔!!!' + OriginTitle;
+         document.title = '✧٩(ˊωˋ*)و' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
